a181b689efb7836dc3129a69e7e4002c71793f8f
Luiza Alencar <66708154+luizadealencar@users.noreply.github.com>
2024-12-01 16:10:53 -0300

    Delete backend/urls.js

--- backend/urls.js
+++
@@ -1,4 +0,0 @@
-export const AMAZON_URL= [
- "https://www.amazon.com.br/Jogo-Copos-Nadir-Dubai-Incolor/dp/B07CMSXPW1/ref=asc_df_B07CMSXPW1/?tag=googleshopp06-20&linkCode=df0&hvadid=647392279416&hvpos=&hvnetw=g&hvrand=6432332989169778125&hvpone=&hvptwo=&hvqmt=&hvdev=m&hvdvcmdl=&hvlocint=&hvlocphy=9196955&hvtargid=pla-2274306420693&psc=1&mcid=639ab92484d8325f953f87da60a669dd",
- "https://www.amazon.com.br/Kit-Potes-Herm%C3%A9ticos-Pl%C3%A1stico-Electrolux/dp/B09XJL4B9H/ref=asc_df_B09XJL4B9H/?tag=googleshopp06-20&linkCode=df0&hvadid=379725468927&hvpos=&hvnetw=g&hvrand=16485426212896812966&hvpone=&hvptwo=&hvqmt=&hvdev=m&hvdvcmdl=&hvlocint=&hvlocphy=9101040&hvtargid=pla-1662902312395&mcid=93036d57993a35b7af417811b1a3528a&th=1"
-]
